e0d21fda24336c074c6ca5fc7a329a154bb2b726
Bradley Bodine <bbodine1@gmail.com>
2018-03-13 20:15:21 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Webpack Starter Project
+
+### Install
+- clone this repo
+- cd into the cloned folder
+- run `npm i`
+
+### Running the project
+- run `npm start` for the local dev server on port 3000
+
+### Build the project
+- run `npm run build` to build in the "dist/" directory
---
+++ webpack.common.js
@@ -0,0 +1,97 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: [
+      './src/js/index.js',
+      './src/scss/styles.scss'
+    ]
+  },
+  output: {
+    filename: 'js/index.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+    new ExtractTextPlugin({
+      filename: './scss/styles.css'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [{ loader: 'html-loader', options: { minimize: true } }]
+      },
+      {
+        test: /\.(svg|gif|png|jpe?g)/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: './img/[name].[ext]',
+              limit: 10000
+            }
+          },
+          {
+            loader: 'img-loader'
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+                minimize: true
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true,
+                minimize: true,
+                plugins: (loader) => [
+                  require('autoprefixer')()
+                ]
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
+                minimize: true
+              }
+            }
+          ]
+        })
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+    new ExtractTextPlugin({
+      filename: './styles.css'
+    })
+  ],
+};
